0a25f758235907fb1cddab0b5aa3d07bc3d50c93
Carlos Atanes Vences <carlos117g@gmail.com>
2023-12-16 12:07:25 +0000

    wip: generador de cartas, esqueleto

--- src/app.js
+++ src/app.js
@@ -2,7 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Array de valores de cartas
+const valoresCartas = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+// Array de iconos de cartas
+const iconosCartas = ['&diams;', '&hearts;', '&spades;', '&clubs;'];
+
+// Función para obtener una carta aleatoria
+function obtenerCartaAleatoria() {
+  const valorAleatorio = valoresCartas[Math.floor(Math.random() * valoresCartas.length)];
+  const iconoAleatorio = iconosCartas[Math.floor(Math.random() * iconosCartas.length)];
+  return { valor: valorAleatorio, icono: iconoAleatorio };
+}
+
+// Función para mostrar la carta
+function mostrarCarta() {
+  const carta = obtenerCartaAleatoria();
+  document.getElementById('valorCarta').innerHTML = carta.valor;
+  document.getElementById('iconoCarta').innerHTML = carta.icono;
+}
+
+// Mostrar la primera carta al cargar la página
+mostrarCarta();
+
+// Manejador de eventos para el botón
+document.getElementById('btnOtraCarta').addEventListener('click', function() {
+  mostrarCarta();
+});
+
