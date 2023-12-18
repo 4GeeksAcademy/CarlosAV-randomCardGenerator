bfd67ef7ca773deed2b1ebe493f9069a3b36d446
Carlos Atanes Vences <carlos117g@gmail.com>
2023-12-18 20:30:23 +0000

    feat: html, CSS and JS

--- src/app.js
+++ src/app.js
@@ -2,30 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-// Array de valores de cartas
-const valoresCartas = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-// Array de iconos de cartas
-const iconosCartas = ['&diams;', '&hearts;', '&spades;', '&clubs;'];
+
+/////////////////////////
+//                     //
+//        Datos        //
+//                     //
+/////////////////////////
+
+// Array con los valores (Datos)
+const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const icons = ['&diams;', '&hearts;', '&spades;', '&clubs;'];
+const colorMap = { '&diams;': 'text-danger', '&hearts;': 'text-danger', '&spades;': 'text-dark', '&clubs;': 'text-dark' };
+/////////////////////////
+//                     //
+//      Funciones      //
+//                     //
+/////////////////////////
 
 // Función para obtener una carta aleatoria
-function obtenerCartaAleatoria() {
-  const valorAleatorio = valoresCartas[Math.floor(Math.random() * valoresCartas.length)];
-  const iconoAleatorio = iconosCartas[Math.floor(Math.random() * iconosCartas.length)];
-  return { valor: valorAleatorio, icono: iconoAleatorio };
+function getRandomCard() {
+  return { value: cards[Math.floor(Math.random() * cards.length)], icon: icons[Math.floor(Math.random() * icons.length)] };
 }
 
-// Función para mostrar la carta
-function mostrarCarta() {
-  const carta = obtenerCartaAleatoria();
-  document.getElementById('valorCarta').innerHTML = carta.valor;
-  document.getElementById('iconoCarta').innerHTML = carta.icono;
+// Función para mostrar la carta (Ni caso a esta mosntruosidad, usad el ejemplo de Davide que es mas legible)
+function mostrarCarta(card) {
+  document.getElementById('cardValue').innerHTML = card.value;
+  document.getElementById('iconA').innerHTML = document.getElementById('iconB').innerHTML = card.icon
+  document.getElementById('iconA').className = document.getElementById('iconB').className = document.getElementById('iconA').className.replace('text-danger', '').replace('text-dark', '') + ' ' + colorMap[card.icon];
 }
+
+/////////////////////////
+//                     //
+//       Codigo        //
+//                     //
+/////////////////////////
 
 // Mostrar la primera carta al cargar la página
-mostrarCarta();
+mostrarCarta(getRandomCard());
 
-// Manejador de eventos para el botón
-document.getElementById('btnOtraCarta').addEventListener('click', function() {
-  mostrarCarta();
-});
-
+// Asignamos el "onclick" a nuestra funcion
+document.getElementById('btnRepeat').onclick = function() {
+  mostrarCarta(getRandomCard());
+};
